a7c5ba12e8899b07359050c18a776a91c69f9e3a
25HP3064 25HP3064 <25HP3064@G301-CC0192.local>
2025-09-23 18:20:43 +0800

    gg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 "use client";
-import instagram from "@/app/images/instagram.png";
+
 import { useUser } from "@/provider/AuthProvider";
 import { useRouter } from "next/navigation";
-import home from "@/app/images/Home.png";
-import add from "@/app/images/Add.png";
-import search from "@/app/images/Search.png";
-import usericon from "@/app/images/user.png";
 
 const Page = () => {
   const { user } = useUser();
@@ -16,14 +12,6 @@ const Page = () => {
   return (
     <div>
       <div>{user?.email}</div>
-      <img
-        src={instagram.src}
-        className="h-[34px] w-[100px] mt-[56px] ml-[16px]"
-      />
-      <img src={home.src} className="h-[54px] w-[81.5px] " />
-      <img src={search.src} className="h-[54px] w-[81.5px] " />
-      <img src={add.src} className="h-[54px] w-[81.5px] " />
-      <img src={usericon.src} className="h-[54px] w-[81.5px]  " />
     </div>
   );
 };
