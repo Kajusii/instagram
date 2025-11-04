119484447df0e4542730dc62912551c7c932c8b7
25HP3064@nest.edu.mn <khaliun@pinecones-iMac.local>
2025-11-04 15:42:40 +0800

    change

--- src/app/editprofile/page.tsx
+++ src/app/editprofile/page.tsx
@@ -13,7 +13,7 @@ const Page = () => {
   const router = useRouter();
   const [file, setFile] = useState<File | null>(null);
   const [image, setImage] = useState<string | null>(null);
-  const {  token } = useUser();
+  const { token } = useUser();
   const [inputValues, setInputValues] = useState({
     username: "",
     bio: "",
@@ -60,7 +60,7 @@ const Page = () => {
       }),
     });
     if (res.ok) {
-      router.push("/myprofile");
+      router.push("/");
     }
   };
 
@@ -110,15 +110,9 @@ const Page = () => {
       </div>
       <Button onClick={photo}>upload image</Button>
       <div>
-      <img src={image ?? 'fallback-image.jpg'} />
+        <img src={image ?? "fallback-image.jpg"} />
       </div>
-      <Button
-        onClick={() => {
-          editUser();
-        }}
-      >
-        Submit
-      </Button>
+      <Button onClick={editUser}>Submit</Button>
       <Buttom />
     </div>
   );
